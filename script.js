95452c951dbb8b19a0a2dc4d9f8fae1c8bf8f164
Jan Christian Becker <s.ecu@hotmail.de>
2024-02-21 12:31:53 +0100

    Update dark-mode-script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const body = document.querySelector("body");
-const toggle = document.getElementById("toggle");
-toggle.addEventListener("click", () => {
-  toggle.classList.toggle("active");
-  body.classList.toggle("active");
-});
